175242f428ddfc4acd0606ecadfb893e2b48c8a0
nikita-skobov <nskobov.websites@gmail.com>
2019-04-04 22:48:10 -0500

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "parser": "babel-eslint",
+  "plugins": [
+      "import"
+  ],
+  "rules": {
+      "semi": [2, "never"],
+      "linebreak-style": ["off"],
+      "no-console": ["off"],
+  },
+}
