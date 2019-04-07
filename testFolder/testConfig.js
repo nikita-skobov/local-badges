db8fa21fa3da2f3b12b37d8217a54b094dad4612
nikita-skobov <nskobov.websites@gmail.com>
2019-04-06 20:07:29 -0500

    makes testConfig actually work

--- testFolder/testConfig.js
+++ testFolder/testConfig.js
@@ -1,1 +1,21 @@
-module.exports = {}
+module.exports = {
+  badges: [
+    {
+      name: 'some-badge',
+      folder: './',
+      fn: (cliObj) => {
+        const template = 'plastic'
+        const colorB = cliObj.successFlag ? 'green' : 'red'
+        const text = ['build', cliObj.successFlag ? 'passing' : 'failing']
+        return {
+          colorB,
+          template,
+          text,
+        }
+      },
+    },
+  ],
+  defaults: {
+    colorA: 'red',
+  },
+}
