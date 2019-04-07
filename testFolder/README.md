29ef58adbc43a13acab07b9917335de786e893bf
nikita-skobov <nskobov.websites@gmail.com>
2019-04-07 15:38:30 -0500

    adds testFolder readme, and example badges

---
+++ testFolder/README.md
@@ -0,0 +1,4 @@
+# this is not where the tests are
+
+The tests for this repository are located primarily within the lib/ directory.
+This folder is called testFolder because some of the tests require changing directories to test relative path names, so this folder is used in those tests.
--- testFolder/testConfig.js
+++ testFolder/testConfig.js
@@ -1,21 +1,38 @@
 module.exports = {
   badges: [
     {
-      name: 'some-badge',
+      // this badge will be made the same every time
+      // because it uses static properties
+      name: 'boring-badge',
+      folder: './',
+      colorA: '#aaa',
+      colorB: '#bbb',
+      text: ['boring', 'badge'],
+    },
+    {
+      // this badge will be different depending on whether or not
+      // you pass in a --successFlag option when running local-badges
+      name: 'build-status',
       folder: './',
       fn: (cliObj) => {
-        const template = 'plastic'
         const colorB = cliObj.successFlag ? 'green' : 'red'
-        const text = ['build', cliObj.successFlag ? 'passing' : 'failing']
+        const passOrFail = cliObj.successFlag ? 'passing' : 'failing'
+        const text = ['build', passOrFail]
         return {
+          colorA: 'gray',
           colorB,
-          template,
           text,
         }
       },
     },
   ],
+
+  // all badges will have the colorA property be purple unless the individual
+  // badge object overrides the colorA property either as a static property
+  // eg: boring-badge
+  // or if the fn function returns a different colorA property
+  // eg: build-status
   defaults: {
-    colorA: 'red',
+    colorA: 'purple',
   },
 }
