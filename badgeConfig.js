dddcd8261c57c9e6691912f138955a453eeacda7
nikita-skobov <nskobov.websites@gmail.com>
2019-04-06 22:01:28 -0500

    fixes test that failed because of renamed file

--- badgeConfig.js
+++ badgeConfig.js
@@ -1,14 +1,24 @@
-const name = 'some-badge'
+const folder = './badges'
+const fs = require('fs')
 
 module.exports = {
   badges: [
     {
-      name,
-      folder: './badges/',
+      name: 'boring-default-badge',
+      folder,
       fn: () => ({
         colorB: 'purple',
         template: 'plastic',
       }),
+    },
+    {
+      name: 'coverage',
+      folder,
+      fn: (cliObj) => {
+        const coveragePath = cliObj['coverage-path']
+        const cloverData = fs.readFileSync(coveragePath, { encoding: 'UTF-8' })
+        console.log(cloverData)
+      },
     },
   ],
   defaults: {
